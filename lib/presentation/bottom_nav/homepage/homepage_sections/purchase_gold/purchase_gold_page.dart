815326fc789b963469bb78832907ededd7223eff
powanpun <powan.pun77@gmail.com>
2022-07-13 15:20:44 +0545

    minor fixes

--- lib/presentation/bottom_nav/homepage/homepage_sections/purchase_gold/purchase_gold_page.dart
+++ lib/presentation/bottom_nav/homepage/homepage_sections/purchase_gold/purchase_gold_page.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:predictandwin/resources/AppColor/app_colors.dart';
@@ -18,82 +20,87 @@ class _PurchaseGoldPageState extends State<PurchaseGoldPage> {
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
-      body: CustomScrollView(
-        physics: const BouncingScrollPhysics(
-            parent: AlwaysScrollableScrollPhysics()),
-        slivers: <Widget>[
-          const SliverAppBar(
-            elevation: 0,
-            backgroundColor: AppColors.backgroundGrey,
-            automaticallyImplyLeading: true,
-            iconTheme: IconThemeData(color: Colors.black),
-            pinned: true,
-          ),
-          SliverPadding(
-            padding: padding16(),
-            sliver: SliverGrid(
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                mainAxisSpacing: 10,
-                crossAxisSpacing: 10,
-                childAspectRatio: 0.8,
-              ),
-              delegate: SliverChildBuilderDelegate(
-                (context, index) {
-                  return InkWell(
-                    onTap: () {
-                      customProductBottomDialog(
-                          context,
-                          "https://img.freepik.com/free-psd/smartwatch-mockup_439185-553.jpg?t=st=1657274444~exp=1657275044~hmac=645c74f3848ca7bd762341738cb340b2f4261a136316afa3d74f13a4b5eb691b&w=996",
-                          "Product $index",
-                          "115",
-                          "description");
-                    },
-                    child: Container(
-                      decoration: customeBoxDecorationWithShadow(),
-                      child: Column(
-                        children: [
-                          Expanded(
-                            flex: 3,
-                            child: ClipRRect(
-                              borderRadius: const BorderRadius.only(
-                                  topLeft: Radius.circular(16),
-                                  topRight: Radius.circular(16)),
-                              child: Image.asset(
-                                "assets/images/gold-bars.png",
+      body: RefreshIndicator(
+        onRefresh: () async {
+          log("refreshed");
+        },
+        child: CustomScrollView(
+          physics: const BouncingScrollPhysics(
+              parent: AlwaysScrollableScrollPhysics()),
+          slivers: <Widget>[
+            const SliverAppBar(
+              elevation: 0,
+              backgroundColor: AppColors.backgroundGrey,
+              automaticallyImplyLeading: true,
+              iconTheme: IconThemeData(color: Colors.black),
+              pinned: true,
+            ),
+            SliverPadding(
+              padding: padding16(),
+              sliver: SliverGrid(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 10,
+                  crossAxisSpacing: 10,
+                  childAspectRatio: 0.8,
+                ),
+                delegate: SliverChildBuilderDelegate(
+                  (context, index) {
+                    return InkWell(
+                      onTap: () {
+                        customProductBottomDialog(
+                            context,
+                            "https://img.freepik.com/free-psd/smartwatch-mockup_439185-553.jpg?t=st=1657274444~exp=1657275044~hmac=645c74f3848ca7bd762341738cb340b2f4261a136316afa3d74f13a4b5eb691b&w=996",
+                            "Product $index",
+                            "115",
+                            "description");
+                      },
+                      child: Container(
+                        decoration: customeBoxDecorationWithShadow(),
+                        child: Column(
+                          children: [
+                            Expanded(
+                              flex: 3,
+                              child: ClipRRect(
+                                borderRadius: const BorderRadius.only(
+                                    topLeft: Radius.circular(16),
+                                    topRight: Radius.circular(16)),
+                                child: Image.asset(
+                                  "assets/images/gold-bars.png",
+                                ),
                               ),
                             ),
-                          ),
-                          Expanded(
-                              child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              customSubHeading1(
-                                  "Golds ${(index + 1) * 5}", AppColors.black),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Text("Rs. ${(index + 5) * 10}",
-                                      style: GoogleFonts.lato(
-                                          fontSize: 16,
-                                          fontWeight: FontWeight.bold,
-                                          color: AppColors.black)),
-                                ],
-                              ),
-                            ],
-                          ))
-                        ],
+                            Expanded(
+                                child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                customSubHeading1("Golds ${(index + 1) * 5}",
+                                    AppColors.black),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Text("Rs. ${(index + 5) * 10}",
+                                        style: GoogleFonts.lato(
+                                            fontSize: 16,
+                                            fontWeight: FontWeight.bold,
+                                            color: AppColors.black)),
+                                  ],
+                                ),
+                              ],
+                            ))
+                          ],
+                        ),
                       ),
-                    ),
-                  );
-                },
-                childCount: 8,
+                    );
+                  },
+                  childCount: 8,
+                ),
               ),
             ),
-          ),
 
-          // other sliver widgets
-        ],
+            // other sliver widgets
+          ],
+        ),
       ),
     ));
   }
--- lib/presentation/bottom_nav/winners/winners.dart
+++ lib/presentation/bottom_nav/winners/winners.dart
@@ -4,6 +4,7 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:predictandwin/resources/appColor/app_colors.dart';
 import 'package:predictandwin/utils/custom_text.dart';
 import 'package:predictandwin/utils/custome_sized_box.dart';
+import 'package:predictandwin/utils/padding.dart';
 
 class WinnersPage extends StatefulWidget {
   const WinnersPage({Key? key}) : super(key: key);
@@ -17,102 +18,108 @@ class _WinnersPageState extends State<WinnersPage> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-          body: SingleChildScrollView(
-              child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            customHeading32("Leader board", AppColors.black),
-            customSizedBoxh16(),
-            Wrap(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: const [
-                    LeaderBoardItem(
-                      playerName: "Sonahang Rai",
-                      gold: "900",
-                      rank: "2",
-                      margin: 0.0,
-                    ),
-                    LeaderBoardItem(
-                      playerName: "Powan Pun",
-                      gold: "1000",
-                      rank: "1",
-                      margin: 60,
-                    ),
-                    LeaderBoardItem(
-                      playerName: "Shyam",
-                      gold: "800",
-                      rank: "3",
-                      margin: 0,
-                    ),
-                  ],
-                ),
-              ],
-            ),
-            ListView.builder(
-              physics: const NeverScrollableScrollPhysics(),
-              scrollDirection: Axis.vertical,
-              shrinkWrap: true,
-              itemCount: 50,
-              itemBuilder: (context, index) {
-                return Container(
-                    margin: const EdgeInsets.only(bottom: 8),
-                    child: Row(
-                      children: [
-                        Expanded(
-                          flex: 1,
-                          child: Text((index + 4).toString(),
-                              style: GoogleFonts.lato(
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.bold,
-                                  color: AppColors.blackLight)),
-                        ),
-                        customSizedBoxw16(),
-                        ClipRRect(
-                          borderRadius: BorderRadius.circular(100),
-                          child: SizedBox(
-                            height: 60,
-                            width: 60,
-                            child: Image.network(
-                              "https://cdn.pixabay.com/photo/2021/10/23/19/25/cat-6735840_960_720.jpg",
-                              fit: BoxFit.cover,
-                            ),
+          body: CustomScrollView(
+              physics: const BouncingScrollPhysics(
+                  parent: AlwaysScrollableScrollPhysics()),
+              slivers: [
+            SliverToBoxAdapter(
+              child: Column(
+                children: [
+                  customHeading32("Leader board", AppColors.black),
+                  customSizedBoxh16(),
+                  Wrap(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: const [
+                          LeaderBoardItem(
+                            playerName: "Sonahang Rai",
+                            gold: "900",
+                            rank: "2",
+                            margin: 0.0,
                           ),
-                        ),
-                        customSizedBoxw16(),
-                        Expanded(
-                          flex: 10,
-                          child: Text("player",
-                              style: GoogleFonts.lato(
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.black)),
-                        ),
-                        Row(
-                          children: [
-                            Image.asset(
-                              "assets/images/gold-bars.png",
-                              scale: 4,
-                            ),
-                            const SizedBox(
-                              width: 8,
-                            ),
-                            Text("115",
-                                style: GoogleFonts.lato(
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.bold,
-                                    color: AppColors.black)),
-                          ],
-                        ),
-                      ],
-                    ));
-              },
+                          LeaderBoardItem(
+                            playerName: "Powan Pun",
+                            gold: "1000",
+                            rank: "1",
+                            margin: 60,
+                          ),
+                          LeaderBoardItem(
+                            playerName: "Shyam",
+                            gold: "800",
+                            rank: "3",
+                            margin: 0,
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
-          ],
-        ),
-      ))),
+            SliverPadding(
+              padding: padding16(),
+              sliver: SliverList(
+                delegate: SliverChildBuilderDelegate(
+                  (BuildContext context, int index) {
+                    return InkWell(
+                        child: Container(
+                            margin: const EdgeInsets.only(bottom: 8),
+                            child: Row(
+                              children: [
+                                Expanded(
+                                  flex: 1,
+                                  child: Text((index + 4).toString(),
+                                      style: GoogleFonts.lato(
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.bold,
+                                          color: AppColors.blackLight)),
+                                ),
+                                customSizedBoxw16(),
+                                ClipRRect(
+                                  borderRadius: BorderRadius.circular(100),
+                                  child: SizedBox(
+                                    height: 60,
+                                    width: 60,
+                                    child: Image.network(
+                                      "https://cdn.pixabay.com/photo/2021/10/23/19/25/cat-6735840_960_720.jpg",
+                                      fit: BoxFit.cover,
+                                    ),
+                                  ),
+                                ),
+                                customSizedBoxw16(),
+                                Expanded(
+                                  flex: 10,
+                                  child: Text("player",
+                                      style: GoogleFonts.lato(
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.black)),
+                                ),
+                                Row(
+                                  children: [
+                                    Image.asset(
+                                      "assets/images/gold-bars.png",
+                                      scale: 4,
+                                    ),
+                                    const SizedBox(
+                                      width: 8,
+                                    ),
+                                    Text("115",
+                                        style: GoogleFonts.lato(
+                                            fontSize: 16,
+                                            fontWeight: FontWeight.bold,
+                                            color: AppColors.black)),
+                                  ],
+                                ),
+                              ],
+                            )));
+                  },
+                  childCount: 10,
+                ),
+              ),
+            ),
+          ])),
     );
   }
 }
